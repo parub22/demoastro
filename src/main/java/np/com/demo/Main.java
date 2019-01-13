ea7fd7ed1bf32e1b4a522f0d6bc34fa3a89bcb75
Parbati Budhathoki <parbatibudhathoki3@gmail.com>
2019-01-13 14:25:39 +0545

    initial commit

---
+++ src/main/java/np/com/demo/Main.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package np.com.demo;
+
+/**
+ *
+ * @author Parbati Budhathoki <parbati@moco.com.np>
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+    }
+    
+}
